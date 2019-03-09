3f67304ad47208c775ae8caf901144d4b57532d7
Pedro <pedro.herranz.escudero@alumnos.upm.es>
2019-03-09 11:52:21 +0100

    Cambio 1 Jess pag24,25

--- hello.rb
+++ hello.rb
@@ -1,5 +1,5 @@
-# Default is "Worldi"
+# Default is "Worldi" --  cambio de Jess en pag24 tras clonacion de Ana
 
-name = ARGV.first || "Worldi"
+name = ARGV.first || "World in 24"
 
 puts "Hello, #{name}!"
